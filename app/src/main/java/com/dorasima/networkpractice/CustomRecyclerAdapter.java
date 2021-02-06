7d72027d098e31e102ba0d30f472db08bbd651b9
Dorasima-98 <cjk1516@gmail.com>
2021-02-06 23:14:05 +0900

    ~ How to make recycler interface 1. create Interface as a customclickListner method, 2. define Interface ins ViewHolder generator 3 use on onCreate

---
+++ app/src/main/java/com/dorasima/networkpractice/CustomRecyclerAdapter.java
@@ -0,0 +1,83 @@
+package com.dorasima.networkpractice;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+// 리스트의 어뎁터
+public class CustomRecyclerAdapter extends RecyclerView.Adapter<CustomRecyclerAdapter.ViewHolder>{
+    private ArrayList<HashMap<String, Object>> mDataList = null;
+    // 클래스 내부의 리스너를 담을 변수
+    private OnItemClickListener onItemClickListener;
+
+    // ViewHolder에서 OnclickListener를 implement 한다.
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        protected final ImageView imageView;
+        protected final TextView textView3;
+
+        ViewHolder(View view,OnItemClickListener onItemClickListener){
+            super(view);
+            imageView = (ImageView)view.findViewById(R.id.imageView);
+            textView3 = (TextView)view.findViewById(R.id.textView3);
+
+            // 리스너를 적용한다.
+            view.setOnClickListener(this);
+        }
+        @Override
+        // onClick을 오버라이딩하는데
+        public void onClick(View view) {
+            // 선택된 뷰 홀더의 인덱스를 얻고
+            int pos = getAdapterPosition();
+            if(pos != RecyclerView.NO_POSITION){
+                if(onItemClickListener!=null){
+                    // 내부 인터페이스의 onItemClick을 호출한다.
+                    onItemClickListener.onItemClick(view,pos);
+                }
+            }
+        }
+    }
+    // 내부 리스너를 적용하는 함수
+    public void setOnItemClickListener(OnItemClickListener listener){
+        this.onItemClickListener = listener;
+    }
+    // 인터페이스로 사용할, onItemClick 메서드
+    public interface OnItemClickListener{
+        void onItemClick(View v,int position);
+    }
+
+    public CustomRecyclerAdapter(ArrayList<HashMap<String,Object>> list){
+        mDataList = list;
+    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_of_list, parent, false);
+        ViewHolder viewHolder= new ViewHolder(view,onItemClickListener);
+
+        return viewHolder;
+    }
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        HashMap<String, Object> map = mDataList.get(position);
+
+        int mobile_img = (Integer)map.get("mobile_image");
+        String mobile_str1 = (String)map.get("mobile_str1");
+
+        holder.imageView.setImageResource(mobile_img);
+        holder.textView3.setText(mobile_str1);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataList.size();
+    }
+
+}
--- app/src/main/java/com/dorasima/networkpractice/MyNetworkProject.java
+++ app/src/main/java/com/dorasima/networkpractice/MyNetworkProject.java
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -20,7 +21,12 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class MyNetworkProject extends AppCompatActivity {
+public class MyNetworkProject extends AppCompatActivity implements CustomRecyclerAdapter.OnItemClickListener {
+
+    @Override
+    public void onItemClick(View v, int position) {
+        Log.d("test","clicked "+position);
+    }
 
     // 리스트뷰 구성을 위해서 필요한 데이터가 담겨 있는 ArrayList
     ArrayList<HashMap<String, Object>> listData = new ArrayList<HashMap<String, Object>>();
@@ -37,9 +43,18 @@ public class MyNetworkProject extends AppCompatActivity {
         // LinearLayoutManager를 세팅해주는 것을 잊지말자.
         main_list.setLayoutManager(new LinearLayoutManager(this));
         CustomRecyclerAdapter adapter = new CustomRecyclerAdapter(listData);
+        // 리싸이클러 아이템 리스너는 이렇게 사용한다.
+        adapter.setOnItemClickListener(new CustomRecyclerAdapter.OnItemClickListener(){
+            @Override
+            public void onItemClick(View v, int position) {
+                Intent detail_intent = new Intent(MyNetworkProject.this,DetailActivity.class);
+                startActivity(detail_intent);
+            }
+        });
         main_list.setAdapter(adapter);
         // adapter.notifyDataSetChanged();
     }
+
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
@@ -55,9 +70,9 @@ public class MyNetworkProject extends AppCompatActivity {
             default:
                 break;
         }
-
         return super.onOptionsItemSelected(item);
     }
+
 
     // 데이터 초기화 메서드
     public void init(){
@@ -79,53 +94,6 @@ public class MyNetworkProject extends AppCompatActivity {
         listData.add(map3);
         // 나중에 지울 것이다.
     }
-    // 리스트의 어뎁터
-    public class CustomRecyclerAdapter extends RecyclerView.Adapter<CustomRecyclerAdapter.ViewHolder>{
-        private ArrayList<HashMap<String, Object>> mDataList = null;
-
-        public class ViewHolder extends RecyclerView.ViewHolder{
-            protected final ImageView imageView;
-            protected final TextView textView3;
-
-            ViewHolder(View view){
-                super(view);
-
-                imageView = (ImageView)view.findViewById(R.id.imageView);
-                textView3 = (TextView)view.findViewById(R.id.textView3);
-            }
-        }
-        public CustomRecyclerAdapter(ArrayList<HashMap<String,Object>> list){
-            mDataList = list;
-        }
-        public CustomRecyclerAdapter(){
-            mDataList = null;
-        }
-        @NonNull
-        @Override
-        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_of_list,parent,false);
-            ViewHolder viewHolder= new ViewHolder(view);
-
-            return viewHolder;
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-            HashMap<String, Object> map = listData.get(position);
-
-            int mobile_img = (Integer)map.get("mobile_image");
-            String mobile_str1 = (String)map.get("mobile_str1");
-
-            holder.imageView.setImageResource(mobile_img);
-            holder.textView3.setText(mobile_str1);
-        }
-
-        @Override
-        public int getItemCount() {
-            return mDataList.size();
-        }
-    }
-
     // 메인 메뉴 구성
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -133,4 +101,5 @@ public class MyNetworkProject extends AppCompatActivity {
         menuInflater.inflate(R.menu.main_menu,menu);
         return true;
     }
+
 }
